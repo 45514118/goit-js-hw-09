d7e3a0b298f826bfb607c04adbf252dab70acae6
Dinar Agzyamov <mail@deenar.ru>
2022-02-04 13:16:34 +0300

    add parcel & start files & done homework 9.1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,23 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+let colorID;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startButton.addEventListener('click', onPressStartButton);
+
+function onPressStartButton() {
+  colorID = setInterval(() => {
+    startButton.disabled = true;
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+stopButton.addEventListener('click', onPressStopButton);
+function onPressStopButton() {
+  startButton.disabled = false;
+  clearInterval(colorID);
+}
