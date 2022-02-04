704d8a2438ab6e7b7de96b4477c70d817f527a81
Dinar Agzyamov <mail@deenar.ru>
2022-02-04 22:03:51 +0300

    add 'create promise' function

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,24 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', onSubmit);
+
+createPromise(position, delay)
+  .then(({ position, delay }) => {
+    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  let promiseValue = { position, delay };
+  return new Promise((resolve, reject) => {
+    if (shouldResolve) {
+      shouldResolve(promiseValue);
+    } else {
+      reject(promiseValue);
+    }
+  });
 }
